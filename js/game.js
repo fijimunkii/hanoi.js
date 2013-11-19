9553c7a897528da9eed8159b74506591986e6d66
Harrison Powers <fijimunkii@gmail.com>
2013-11-19 12:16:54 -0500

    disable scrolling

--- js/game.js
+++ js/game.js
@@ -142,4 +142,15 @@ $(function() {
     onlyEnableTopDiscs();
   }, 50);
 
+  // lock scroll position
+  var scrollPosition = [
+    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+    self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+  ];
+  var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+  html.data('scroll-position', scrollPosition);
+  html.data('previous-overflow', html.css('overflow'));
+  html.css('overflow', 'hidden');
+  window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
 });
